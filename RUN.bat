85c1378d58f61b62da058faf667d625acc63826a
Tomáš Škraban <tom-00x@email.cz>
2022-11-15 13:46:38 +0100

    bats modified pyvenv -> venv

--- RUN.bat
+++ RUN.bat
@@ -1,11 +1,2 @@
 echo off
-echo Ve stejne slozce jako je tento skript musi byt soubor s tapetou 'wallpaper.jpg'.
-echo Zadejte prihlasovaci udaje do bakalaru. Je to jen jednorazove a nikde se neukladaji.
-echo Pokud se vse povede, vyskoci systemova notifikace a zmeni se tapeta.
-echo.
-
-set /p un="Jmeno: "
-set /p ps="Heslo: "
-pyvenv\Scripts\pythonw.exe source\main.py %un% %ps%
-echo Program spusten. Bezi na pozadi.
-pause
+start venv\Scripts\pythonw.exe source\main.py
